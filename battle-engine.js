83cdf8b3b8def2f410fc6e3d36e59fd9ddef98d7
sparktrain <lukas.blakkan.esser@gmail.com>
2014-05-29 14:10:29 -0400

    various bug fixes / updates
    to keep server up to date with the main one

--- battle-engine.js
+++ battle-engine.js
@@ -2580,7 +2580,7 @@ var Battle = (function () {
 					boost[i] = -boost[i];
 				}
 				switch (effect.id) {
-				case 'intimidate':
+				case 'intimidate': case 'gooey':
 					this.add(msg, target, i, boost[i]);
 					break;
 				default:
